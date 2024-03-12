f8bd7c9186f6d13aa90522540a734ecca1a85eab
Lika-l <114182483+Lika-l@users.noreply.github.com>
2024-03-12 22:13:23 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,13 +1,9 @@
 function ChangeLogo() {
     const logo = document.getElementById('logo');
-    
-
-
-    
-    if (logo.src.includes('googlelogo_color_272x92dp.png')) {
-        logo.src = './img/new_logo.png'; // Путь к новому логотипу
+  if (logo.src.includes('googlelogo_color_272x92dp.png')) {
+        logo.src = './img/new_logo.png';
     } else {
-        logo.src = './img/googlelogo_color_272x92dp.png'; // Путь к изначальному логотипу
+        logo.src = './img/googlelogo_color_272x92dp.png'; 
     }
 
 }
